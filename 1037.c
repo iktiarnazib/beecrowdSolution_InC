9fc2d0a0a80be9d198d7968019fcb04c46299a75
iktiarnazib <108393154+iktiarnazib@users.noreply.github.com>
2023-05-22 21:00:06 +0600

    Updated 1037.c - parenthesis Fix
    Updated 1037.c - parenthesis Fix

--- 1037.c
+++ 1037.c
@@ -5,7 +5,7 @@ main()
     double a;
     scanf("%lf", &a);
     if(a>=0 && a<=25){
-        printf("Intervalo (0,25]\n");
+        printf("Intervalo [0,25]\n");
     }else if(a>25 && a<=50){
             printf("Intervalo (25,50]\n");
     }else if(a>50 && a<=75){
